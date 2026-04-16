3519e17fe0ebab899352180da911bb6de5cf8e50
Lisa Vercruysse <lisavercruysse.dev@gmail.com>
2026-04-16 08:54:51 +0200

    prognostic post

--- src/main/java/com/example/teampredictionworldcup/controller/PrognosticController.java
+++ src/main/java/com/example/teampredictionworldcup/controller/PrognosticController.java
@@ -1,17 +1,29 @@
 package com.example.teampredictionworldcup.controller;
 
 import com.example.teampredictionworldcup.dto.response.MatchMinimalDTO;
+import com.example.teampredictionworldcup.dto.response.PrognosticDTO;
+import com.example.teampredictionworldcup.dto.response.PrognosticInputDTO;
 import com.example.teampredictionworldcup.service.PrognosticService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/prognostics")
 public class PrognosticController {
+    private final PrognosticService prognosticService;
 
+    @GetMapping("/form/{matchId}/{memberId}")
+    public String showForm(@PathVariable int matchId, @PathVariable int memberId, Model model) {
+        model.addAttribute("inputDTO", new PrognosticInputDTO(null, null, matchId, memberId));
+        return "prognosticForm";
+    }
+
+    @PostMapping
+    public String processForm(PrognosticInputDTO inputDTO) {
+        prognosticService.save(inputDTO);
+        return "redirect:/home";
+    }
 }
