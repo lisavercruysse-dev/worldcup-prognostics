ea50b43d72c1b3f2da46d7f2a4d74851baa3f9d6
Lisa Vercruysse <lisavercruysse.dev@gmail.com>
2026-05-25 15:40:50 +0200

    Testen Security

--- src/main/java/com/example/teampredictionworldcup/config/SecurityConfig.java
+++ src/main/java/com/example/teampredictionworldcup/config/SecurityConfig.java
@@ -3,17 +3,8 @@ package com.example.teampredictionworldcup.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -25,9 +16,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/login**", "/", "/home", "/css/**", "/403**", "/matches/{matchId}", "/teams/leaderboard/**").permitAll()
-                        .requestMatchers("/matches", "/matches/form/**", "/matches/{matchId}/score", "/stadiums/**").hasRole("ADMIN")
-                        .anyRequest().authenticated())
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login**", "/", "/home", "/css/**", "/403**", "/teams/leaderboard/**").permitAll()
+                        .requestMatchers("/matches/form", "/matches/form/**", "/matches/*/score", "/stadiums/**", "/matches").hasRole("ADMIN")
+                        .requestMatchers("/matches/*").permitAll()
+                        .anyRequest().hasRole("USER"))
                 .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/", true).usernameParameter("username").passwordParameter("password"))
                 .exceptionHandling(ex -> ex.accessDeniedPage("/403"))
                 .logout(logout -> logout
